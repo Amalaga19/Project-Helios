7dc89a85b517372eb49a495910fd329b30f3c696
Amalaga19 <andres.malaga1@gmail.com>
2024-06-30 01:04:49 +0200

    Changed structure of places_dict to wrap categories and subcategories in two separate keys so it can be handled easier with our current frontend. Chnaged category names and colors of categorycolors function.

--- frontend/Barter-Dashboard/app/dashboard/components/home/utils.js
+++ frontend/Barter-Dashboard/app/dashboard/components/home/utils.js
@@ -1,15 +1,15 @@
 // utils.js
 export const categoryColors = {
-  restaurant: 'red',
-  cafe: 'blue',
-  store: 'green',
-  office: 'yellow',
-  service: 'purple',
-  multicolored: 'orange', // For businesses with multiple categories
+  catering: 'Coral',
+  commercial: 'MediumAquamarine',
+  production: 'RoyalBlue',
+  office: 'DeepPink',
+  service: 'indigo',
+  multiple: 'orange', // For businesses with multiple categories
 };
 
 export const getColorForCategory = (categories) => {
-  if (categories.length === 1) {
+  if (categories.length > 1) {
     return categoryColors[categories[0]] || 'grey';
   }
   return categoryColors.multicolored;
